b4ac3bdaf8dd52e2a812352cd1d7f794fc971d0f
Alan Grow <alangrow@gmail.com>
2011-02-04 19:30:52 -0500

    add benchmark data header

---
+++ benchmark-data.h
@@ -0,0 +1,16 @@
+#ifndef __BENCHMARK_DATA_H__
+#define __BENCHMARK_DATA_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const char *testdata_article[];
+extern const char *testdata_stopwords[];
+
+#ifdef __cplusplus
+};  /* extern "C" */
+#endif
+
+#endif // __BENCHMARK_DATA_H__
+
